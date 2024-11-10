613d73361346ffce025fa6f7bb64c4c24ca1f485
NazihGara <nazihgaraali@gmail.com>
2024-11-10 15:42:34 +0100

    add pass

--- src/main/java/tn/esprit/tpfoyer17/TpFoyer17Application.java
+++ src/main/java/tn/esprit/tpfoyer17/TpFoyer17Application.java
@@ -2,9 +2,13 @@ package tn.esprit.tpfoyer17;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //@EnableScheduling
 @SpringBootApplication
+@EntityScan(basePackages = "tn.esprit.tpfoyer17.entities")
+@EnableJpaRepositories(basePackages = "tn.esprit.tpfoyer17.repositories")
 public class TpFoyer17Application {
 
     public static void main(String[] args) {
