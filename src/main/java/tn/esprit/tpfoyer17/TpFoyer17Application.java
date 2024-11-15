29d6b492b48fba04d2e333b53a25e9892fa0a841
HJIRI Amir <115500830+hjiriamir@users.noreply.github.com>
2024-11-15 01:34:52 +0100

    Update TpFoyer17Application.java

--- src/main/java/tn/esprit/tpfoyer17/TpFoyer17Application.java
+++ src/main/java/tn/esprit/tpfoyer17/TpFoyer17Application.java
@@ -2,6 +2,8 @@ package tn.esprit.tpfoyer17;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //@EnableScheduling
 @SpringBootApplication
